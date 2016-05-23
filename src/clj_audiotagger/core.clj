fad91e739f337e3488fef58a91d326a205cd40fc
Burin Choomnuan <agilecreativity@gmail.com>
2016-05-23 16:00:48 -0400

    Initial import

---
+++ src/clj_audiotagger/core.clj
@@ -0,0 +1,6 @@
+(ns clj-audiotagger.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_audiotagger/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-audiotagger.core-test
+  (:require [clojure.test :refer :all]
+            [clj-audiotagger.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
